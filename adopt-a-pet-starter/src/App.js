2543459d68580c542090152cda0a3b54caa87aa9
andrew <casal.andrew124@gmail.com>
2022-07-11 20:17:24 -0400

    14. imported Switch

--- adopt-a-pet-starter/src/App.js
+++ adopt-a-pet-starter/src/App.js
@@ -3,7 +3,7 @@ import SearchPage from './pages/search';
 import PetDetailsPage from './pages/detail';
 import PetDetailsNotFound from './pages/petDetailsNotFound';
 import Navigation from './components/navigation';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
