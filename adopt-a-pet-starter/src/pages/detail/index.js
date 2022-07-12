25e1b0514bb5cb74feba75acf1ca9cd74d2a11a2
andrew <casal.andrew124@gmail.com>
2022-07-11 20:49:23 -0400

    24. imported Redirect

--- adopt-a-pet-starter/src/pages/detail/index.js
+++ adopt-a-pet-starter/src/pages/detail/index.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { getPetDetails } from '../../api/petfinder';
 import Hero from '../../components/hero';
-import { useParams } from 'react-router-dom';
+import { useParams, Redirect } from 'react-router-dom';
 
 const PetDetailsPage = () => {
   const [data, setData] = useState();
