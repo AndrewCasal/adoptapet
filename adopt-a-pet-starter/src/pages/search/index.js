9178a617140bdd42144204ec90dfcb843fde4b6e
andrew <casal.andrew124@gmail.com>
2022-07-11 20:32:48 -0400

    19. imported useLocation

--- adopt-a-pet-starter/src/pages/search/index.js
+++ adopt-a-pet-starter/src/pages/search/index.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect, useMemo } from 'react';
 import Hero from '../../components/hero';
 import { getPets } from '../../api/petfinder';
 import Pet from '../../components/pet';
+import { useLocation } from 'react-router-dom';
 
 // import useLocation here
 
